4a4b6bd3a482d8f8c9f6aa4af52a8898a8ef3a83
raaynaldo <raaynaldo@hotmail.com>
2020-10-25 16:24:26 -0400

    Done.

--- app/models/show.rb
+++ app/models/show.rb
@@ -1,3 +1,14 @@
 class Show < ActiveRecord::Base
-  
+  has_many :characters
+  has_many :actors, through: :characters
+  belongs_to :network
+
+  def call_letter
+  end
+
+  def actors_list
+    self.actors.map { |actor|
+      actor.full_name
+    }
+  end
 end
